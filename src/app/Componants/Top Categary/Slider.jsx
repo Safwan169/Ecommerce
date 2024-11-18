73c73f23ec0916715f92479168b841a28220bda8
safwan169 <you@example.com>
2024-11-18 18:57:43 +0600

    change img ration

--- src/app/Componants/Top Categary/Slider.jsx
+++ src/app/Componants/Top Categary/Slider.jsx
@@ -58,7 +58,7 @@ export default function Slider({ d }) {
                 {d?.map((data, index) => <SwiperSlide key={index}>
                     <div className=' group  text-[14px] py-3 w-full space-y-5 '>
                         <div className='w-[50] h-fit mx-auto'>
-                            <Image width={50} height={50} className=' group-hover:scale-110 duration-500 w-[50px]' src={data.icon} alt="" />
+                            <Image width={600} height={600} className=' group-hover:scale-110 duration-500 w-[50px]' src={data.icon} alt="" />
 
                         </div>
                         <span className='flex transition-all group-hover:text-red-600 duration-300 mx-auto w-[100px]   justify-center'>
