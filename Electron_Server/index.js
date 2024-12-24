31a4e3d9ce71ec4d1d54755fc4a3284ff362d4c9
safwan 169 <safwanhossainmridul@gmail.com>
2024-12-24 22:48:57 +0600

    get data from backend

--- Electron_Server/index.js
+++ Electron_Server/index.js
@@ -4,6 +4,15 @@ const port = process.env.PORT || 5000;
 
 require('dotenv').config()
 
+const cors= require('cors')
+app.use(cors(
+  {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true
+  }
+
+));
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -18,18 +27,36 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+async function run () {
+   // Connect the client to the server	(optional starting in v4.7)
+  //  await client.connect();
    const database= await client.db("Electron")
+   const productsData = await database.collection("productsData")
+  try {
+   
+
+   
+  app.get("/CardData",async(req,res)=>{
+
+     const data = await productsData.find().toArray()
+
+     console.log(data,'this is the data')
+     const products =  data?.flatMap(d=>d.products)
+
+    res.send({products})
+
+
+  })
+
+
 
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
+
+run().catch(console.error);
 
 
 app.get('/', (req, res) => {
