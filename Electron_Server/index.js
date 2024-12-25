6631d0271e0a8b18b7066cd04efc92bfbca9264f
safwan 169 <safwanhossainmridul@gmail.com>
2024-12-25 14:06:08 +0600

    featured funtionality done

--- Electron_Server/index.js
+++ Electron_Server/index.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
 
-require('dotenv').config()
+require("dotenv").config();
 
-const cors= require('cors')
-app.use(cors(
-  {
+const cors = require("cors");
+app.use(
+  cors({
     origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true
-  }
+    credentials: true,
+  })
+);
 
-));
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.6zehkma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.6zehkma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,58 +22,73 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-async function run () {
-   // Connect the client to the server	(optional starting in v4.7)
+async function run() {
+  // Connect the client to the server	(optional starting in v4.7)
   //  await client.connect();
-   const database= await client.db("Electron")
-   const productsData = await database.collection("productsData")
+  const database = await client.db("Electron");
+  const productsData = await database.collection("productsData");
   try {
-   
+    app.get("/CardData/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
 
-   
-  app.get("/CardData/:id",async(req,res)=>{
-
-
-    const id =req.params.id
-    console.log(id)
-
-    let data
-    if (id ==0){
-
-      
-      data = await productsData.aggregate([
-      {$unwind:'$products'},
-      {$sort:{'products.add_date':-1}},
-      {
-        $project:{
-          products:'$products'
-        }
+      let data;
+      if (id == 0) {
+        // this is for new products section 
+        data = await productsData
+          .aggregate([
+            { $unwind: "$products" },
+            { $sort: { "products.add_date": -1 } },
+            {
+              $project: {
+                products: "$products",
+              },
+            },
+          ])
+          .toArray();
       }
-     ]).toArray()
+      if (id == 1) {
 
-    }
-    if(id==1){
+        // this is for featured products section 
+         data = await productsData
+        .aggregate([
+          { $unwind: "$products" }, // Unwind the products array
+          { 
+            $match: { 
+              "products.isFeatured": "true" // Filter for isFeatured = true
+            } 
+          },
+          { $sort: { "products.add_date": -1 } }, // Sort by add_date in descending order
+          { 
+            $project: { 
+              products: "$products" // Project only the products field
+            } 
+          }
+        ])
+        .toArray();
+      
+      console.log(data);
+      
 
-    }
-    if(id==2){
+        // console.log(da,"this is for featured products ")
 
 
-    }
+      }
+      if (id == 2) {
 
+        // this is for best seller button when payment is done then i add a field  which is sell product then i found the product which was the best seller product
+      }
 
-    const products =  data?.flatMap(d=>d.products)
-    console.log(products,'this is the data')
+      const products = data?.flatMap((d) => d.products);
 
-    res.send({products})
+      console.log(data,'this is the data for the products')
+      console.log(products, "this is the data");
 
-
-  })
-
-
-
+      res.send({ products });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -84,11 +97,10 @@ async function run () {
 
 run().catch(console.error);
 
-
-app.get('/', (req, res) => {
-  res.send(`Yeah baby you are connected`)
-})
+app.get("/", (req, res) => {
+  res.send(`Yeah baby you are connected`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- src/app/Componants/TreandingCollection/TrendingCollectionCard.jsx
+++ src/app/Componants/TreandingCollection/TrendingCollectionCard.jsx
@@ -30,7 +30,7 @@ const TrendingCollectionCard = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [trendingProduct]);
 
   if (loading) return <Loading/>
 
